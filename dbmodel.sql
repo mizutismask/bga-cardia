d215ccbc1178492fa5c141176b38e0ae8f140197
mizutismask <severinek@gmail.com>
2025-03-24 15:43:33 +0100

    feat: mimics foreign key between card and token

--- dbmodel.sql
+++ dbmodel.sql
@@ -34,7 +34,7 @@
 -- ALTER TABLE `player` ADD `player_my_custom_field` INT UNSIGNED NOT NULL DEFAULT '0';
 
 CREATE TABLE IF NOT EXISTS `card` (
-    `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `card_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `card_type` varchar(16) NOT NULL,
     `card_type_arg` int(11) NOT NULL,   
     `card_location` varchar(20) NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `token` (
     `card_type` varchar(16) NOT NULL,
     `card_type_arg` int(11) NOT NULL,   
     `card_location` varchar(20) NOT NULL,
-    `card_location_arg` int(11) NOT NULL,
+    `card_location_arg` int(11) unsigned NOT NULL,
     PRIMARY KEY (`card_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
