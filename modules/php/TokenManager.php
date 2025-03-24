81e03462786511ceeba1a6a188760c2ddc071543
mizutismask <severinek@gmail.com>
2025-03-24 15:42:21 +0100

    feat: add sigil count

--- modules/php/TokenManager.php
+++ modules/php/TokenManager.php
@@ -2,11 +2,26 @@
 
 namespace Bga\Games\Cardia;
 
+use Bga\Games\Cardia\objects\TokenType;
+
 const TABLE_TOKEN = "token";
 
 class TokenManager extends DeckManager {
 
+    public function getSigilCount(int $playerOrder) {
+        $tokenType = TokenType::SIGIL->value;
+        $tokenTable = TABLE_TOKEN;
+        $cardTable = TABLE_CARD;
 
+        $sql = "SELECT count(token.card_id) 
+            FROM $tokenTable as token 
+            JOIN $cardTable as card ON card.card_id = token.card_location_arg
+            WHERE token.card_type = '$tokenType' 
+            AND token.card_location = 'card' 
+            AND card.card_type_arg = '$playerOrder'
+        ";
+        return intval($this->deck->getUniqueValueFromDB($sql));
+    }
     public function pickInitialActionCards() {
         $this->initRiver(4);
     }
