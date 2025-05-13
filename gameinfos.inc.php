6ad7ced6e4a8f0804d32b88148b65ba62fe454d6
mizutismask <severinek@gmail.com>
2025-05-13 11:56:30 +0200

    feat: update gameinfos

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -20,7 +20,7 @@ $gameinfos = [
     'publisher_website' => 'https://www.hans-im-glueck.de/',
 
     // Board Game Geek ID of the publisher
-    'publisher_bgg_id' => 1234,
+    'publisher_bgg_id' => 133,
 
     // Board game geek ID of the game
     'bgg_id' => 0,
@@ -37,7 +37,7 @@ $gameinfos = [
     // 'not_recommend_player_number' => array( 2, 3 ),      // <= example: this is not recommended to play this game with 2 or 3 players
 
     // Estimated game duration, in minutes (used only for the launch, afterward the real duration is computed)
-    'estimated_duration' => 30,
+    'estimated_duration' => 10,
 
     // Time in second add to a player when "giveExtraTime" is called (speed profile = fast)
     'fast_additional_time' => 30,
