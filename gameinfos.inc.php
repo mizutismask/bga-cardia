d952b0211e065f416eb97639ff0867d5d24aa115
mizutismask <severinek@gmail.com>
2025-05-28 15:41:56 +0200

    feat: add bgg id

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -23,7 +23,7 @@ $gameinfos = [
     'publisher_bgg_id' => 133,
 
     // Board game geek ID of the game
-    'bgg_id' => 0,
+    'bgg_id' => 447243,
 
     // Players configuration that can be played (ex: 2 to 4 players)
     'players' => [2],
